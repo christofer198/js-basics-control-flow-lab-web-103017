4b9f2047b8b22de41bb7f6e51a3a0d5f5ac85f0f
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-12-11 02:16:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,6 +21,10 @@ function switchOnCharmFromTip(tip){
     case 'generous':
       return 'Thank you so much.';
       break;
-
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    default:
+      return 'Bye.'
   }
 }
